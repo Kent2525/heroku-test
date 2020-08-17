a418de469286eaf2180e8975155cbbb27dc77efb
Shimizu Kento <aibo396@yahoo.co.jp>
2020-08-17 06:50:37 +0000

    heroku test

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -72,6 +72,7 @@ class NewsController extends Controller
       $news = News::find($request->id);
       // 送信されてきたフォームデータを格納する
       $news_form = $request->all();
+      
       if ($request->remove == 'true') {
         $news_form['image_path'] = null;
       } elseif ($request->file('image')) {
