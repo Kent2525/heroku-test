5fb9c8ee394f5e51cba6766ff8ef5b4c8d063795
Shimizu Kento <aibo396@yahoo.co.jp>
2020-08-18 12:46:46 +0000

    heroku

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -23,7 +23,7 @@ class NewsController extends Controller
 
       // formに画像があれば、保存する
       if (isset($form['image'])) {
-        // public/imageに画像を保存する（ローカル環境）
+        // public/imageに画像を保存する（ローカル環境から本番環境に変更）
         $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
         // 画像の名前をbasename(省略形)で保存する
         $news->image_path = Storage::disk('s3')->url($path);
@@ -65,49 +65,34 @@ class NewsController extends Controller
       return view ('admin.news.edit', ['news_form' => $news]);
   }
   
-  public function update(Request $request)
-  {
-      // Validationをかける
-      $this->validate($request, News::$rules);
-      // News Modelからデータを取得する
-      $news = News::find($request->id);
-      // 送信されてきたフォームデータを格納する
-      $news_form = $request->all();
-      
-      if ($request->remove == 'true') {
-        $news_form['image_path'] = null;
-      } elseif ($request->file('image')) {
-        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
-        $news_form['image_path'] = Storage::disk('s3')->url($path);
-      } else {
-        $news_form['image_path'] = $news->image_path;
-      }
-      
-      unset($news_form['_token']);
-      unset($news_form['image']);
-      unset($news_form['remove']);
-      $news->fill($news_form)->save();
-      
-      $history = new History;
-      $history->news_id = $news->id;
-      $history->edited_at = Carbon::now();
-      $history->save();
-      // if (isset($news_form['image'])) {
-      //   $path = $request->file('image')->store('public/image');
-      //   $news->image_path = basename($path);
-      //   unset($news_form['image']);
-      // } elseif (isset($request->remove)) {
-      //   $news->image_path = null;
-      //   unset($news_form['remove']);
-      // }
-      // unset($news_form['_token']);
-      // unset($news_form['image']);
-      // // 該当するデータを上書きして保存する
-      // $news->fill($news_form)->save();
+    public function update(Request $request)
+    {
+        $this->validate($request, News::$rules);
+        $news = News::find($request->id);
+        $news_form = $request->all();
+        if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
 
-      return redirect('admin/news/');
-  }
-  
+        unset($news_form['_token']);
+        unset($news_form['image']);
+        unset($news_form['remove']);
+        $news->fill($news_form)->save();
+
+        // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
+        return redirect('admin/news/');
+    }
+ 
   public function delete(Request $request)
   {
       // 該当するNews Modelを取得
